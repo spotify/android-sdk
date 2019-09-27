ac0478e9e28b54ae3b9960f42717cd7ac93bd22a
Manuel del Olmo Pomares <manuelo@spotify.com>
2019-09-27 16:39:53 +0200

    Reformat app-remote-sample according to GJF

--- app-remote-sample/src/main/java/com/spotify/sdk/demo/Echo.java
+++ app-remote-sample/src/main/java/com/spotify/sdk/demo/Echo.java
@@ -4,29 +4,29 @@ import com.spotify.protocol.types.Item;
 
 public interface Echo {
 
-    class Request implements Item {
-        public final String request;
+  class Request implements Item {
+    public final String request;
 
-        public Request(String request) {
-            this.request = request;
-        }
-
-        /* For GSON */
-        public Request() {
-            request = null;
-        }
+    public Request(String request) {
+      this.request = request;
     }
 
-    class Response implements Item {
-        public final String response;
-
-        public Response(String response) {
-            this.response = response;
-        }
-
-        /* For GSON */
-        public Response() {
-            response = null;
-        }
+    /* For GSON */
+    public Request() {
+      request = null;
     }
+  }
+
+  class Response implements Item {
+    public final String response;
+
+    public Response(String response) {
+      this.response = response;
+    }
+
+    /* For GSON */
+    public Response() {
+      response = null;
+    }
+  }
 }
