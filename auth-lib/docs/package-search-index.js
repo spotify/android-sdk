aec766e2c99e0b4610f5875bb6e9c6a1f4ad0cdc
Veronika Figura <veronikap@spotify.com>
2022-03-10 18:02:12 +0100

    Update docs

---
+++ auth-lib/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.spotify.sdk.android.auth"},{"l":"com.spotify.sdk.android.auth.app"},{"l":"com.spotify.sdk.android.auth.browser"}]
